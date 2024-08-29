5bd6a70eabe5d29b4a17b32ef7b527464c2b33e0
Jan Lörwald <loerwald.jan@gmail.com>
2024-08-29 11:59:57 +0200

    Code Update

--- includes/wordpress_menu/menu_admin.php
+++ includes/wordpress_menu/menu_admin.php
@@ -390,16 +390,15 @@ function krp_create_or_update_page() {
             $job_image = esc_url($job['job_image'] ?? '');
             $job_more_image = esc_url($job['job_more_image'] ?? '');
             $job_title = esc_html($job['job_title'] ?? '');
-            // Überprüfen, ob 'job_bereich' im Array $job existiert und ob es ein Array ist
             if (isset($job['job_bereich']) && is_array($job['job_bereich'])) {
                 $job_bereich = implode(' und ', array_map('esc_html', $job['job_bereich']));
+                $job_bereich_create_p_tag = implode('', array_map(function($bereich) {
+                    return '<p class="jbc-single-p-tag">' . esc_html($bereich) . '</p>';
+                }, $job['job_bereich']));
             } else {
-                // Setze eine Standardnachricht oder einen leeren String, falls der Schlüssel nicht existiert oder kein Array ist
-                $job_bereich = 'Keine Angaben vorhanden';
+                $job_bereich = '';
+                $job_bereich_create_p_tag = '';
             }
-            $job_bereich_create_p_tag = implode('', array_map(function($bereich) {
-                return '<p class="jbc-single-p-tag">' . esc_html($bereich) . '</p>';
-            }, $job['job_bereich']));
             $job_id = $index + 1; // ID für Referenz
             $contact_person_job_details_name = esc_html($job['selected_contact_job_details_name']);
             $contact_person_job_details_tel = esc_html($job['selected_contact_job_details_tel']);
