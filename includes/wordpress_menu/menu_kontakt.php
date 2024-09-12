056dee2d0c40a4f10cea0442937daaa257c1d90d
Jan Lörwald <loerwald.jan@gmail.com>
2024-09-12 13:25:43 +0200

    Code Update

--- includes/wordpress_menu/menu_kontakt.php
+++ includes/wordpress_menu/menu_kontakt.php
@@ -249,6 +249,7 @@ function krp_kontakt_select_section_callback() {
 
 function krp_kontakt_create_section_callback() {
     $contacts = get_option('krp_saved_contacts', array());
+    $placeholder_image = plugin_dir_url(__FILE__) . 'assets/img/Platzhalterbild.jpg';
     ?>
 
     <style>
@@ -327,9 +328,10 @@ function krp_kontakt_create_section_callback() {
                                             <button type="button" id="krp-media-select_<?php echo $key; ?>" class="krp-media-select-button" data-contact="<?php echo $key; ?>">Bild auswählen</button>
                                         </div>
                                         <div id="krp-image-preview_<?php echo $key; ?>" class="krp-image-preview-container">
-                                            <?php if (!empty($contact['contact_image_url'])): ?>
-                                                <img src="<?php echo esc_url($contact['contact_image_url']); ?>" alt="Bildvorschau">
-                                            <?php endif; ?>
+                                            <?php
+                                            $image_url = !empty($contact['contact_image_url']) ? esc_url($contact['contact_image_url']) : esc_url($placeholder_image);
+                                            ?>
+                                            <img src="<?php echo $image_url; ?>" alt="Bildvorschau">
                                         </div>
                                         <input type="hidden" id="contact_image_url_<?php echo $key; ?>" name="contact_image_url[]" value="<?php echo esc_attr($contact['contact_image_url']); ?>">
                                     </td>
@@ -523,7 +525,10 @@ function krp_kontakt_create_section_callback() {
 
                 function updatePreview(url, contactIndex) {
                     var previewContainer = document.getElementById('krp-image-preview_' + contactIndex);
-                    previewContainer.innerHTML = '<img src="' + url + '" alt="Bildvorschau">';
+                    var defaultImage = '<?php echo esc_url($placeholder_image); ?>';
+                    var imageUrl = url || defaultImage;
+
+                    previewContainer.innerHTML = '<img src="' + imageUrl + '" alt="Bildvorschau">';
 
                     var hiddenInput = document.getElementById('contact_image_url_' + contactIndex);
                     if (!hiddenInput) {
